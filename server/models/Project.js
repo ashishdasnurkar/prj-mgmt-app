c890ea1636825d6bf96daf6ba807037742c75345
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-06-30 21:15:03 +1000

    update status type to enum

--- server/models/Project.js
+++ server/models/Project.js
@@ -9,6 +9,7 @@ const ProjectSchema = new mongoose.Schema({
   },
   status: {
     type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
   },
   clientId: {
     type: mongoose.Schema.Types.ObjectId,
