5d4628008af1768e727371d367d51205f0be9a0e
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-07-29 20:40:16 +1000

    complete project card component

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
   return (
     <>
       {data.projects.length > 0 ? (
-        <div className="row">
+        <div className="row mt-4">
           {data.projects.map((project) => {
             return <ProjectCard key={project.id} project={project} />;
           })}
