2b4985cf791e59e2573791d7cbc04f75dd626652
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-07-31 06:52:43 +1000

    setup 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 const cache = new InMemoryCache({
   typePolicies: {
     Query: {
@@ -33,6 +34,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
             </Routes>
           </div>
         </Router>
