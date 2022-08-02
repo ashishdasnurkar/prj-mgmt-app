54554a0b3deaea30226df914a885658f7bec7663
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-08-02 20:02:46 +1000

    project comp layout and data display

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -10,5 +10,19 @@ export default function Project() {
   });
   if (loading) return <Spinner />;
   if (error) return <p>Something went wrong!</p>;
-  return <div>Project</div>;
+  return (
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+            Back
+          </Link>
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5>Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+        </div>
+      )}
+    </>
+  );
 }
