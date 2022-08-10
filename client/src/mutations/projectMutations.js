e799d48c5997db9ac727fc01bb48e7eba3a8ae3d
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-08-10 20:24:03 +1000

    update add project mutation to use clientId

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,12 +1,28 @@
 import { gql } from "@apollo/client";
 
 const ADD_PROJECT = gql`
-  mutation addProject($name: String!, $description: String!, $status: String!) {
-    addClient(name: $name, description: $description, status: $status) {
+  mutation addProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addClient(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
       id
       name
       description
       status
+      client {
+        id
+        name
+        email
+        phone
+      }
     }
   }
 `;
